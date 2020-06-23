676792b3a05615fe8da3c930380fee60ea96bb20
isaac-j-miller <44651730+isaac-j-miller@users.noreply.github.com>
2020-06-22 23:19:46 -0400

    Updates

---
+++ precache-manifest.c02bc0fa18f54003ad4f3f5de8998a45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c42515694698c4e1fe0930f8c1914be",
+    "url": "/virtual-hendi/index.html"
+  },
+  {
+    "revision": "51a32d1258e357c2b7c9",
+    "url": "/virtual-hendi/static/css/main.40626981.chunk.css"
+  },
+  {
+    "revision": "5f8bcb07129e2ade6d0e",
+    "url": "/virtual-hendi/static/js/2.5511e04c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/virtual-hendi/static/js/2.5511e04c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51a32d1258e357c2b7c9",
+    "url": "/virtual-hendi/static/js/main.c9239eeb.chunk.js"
+  },
+  {
+    "revision": "020120ae7a200a28194c",
+    "url": "/virtual-hendi/static/js/runtime-main.92dfcbb4.js"
+  }
+]);
